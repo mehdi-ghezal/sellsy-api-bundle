fae7ea02813c454efcc6c5a85b345deadcce4870
Mehdi Ghezal <mehdi.ghezal@gmail.com>
2016-03-02 13:34:16 +0800

    Optional parameter

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -22,7 +22,7 @@ class Configuration implements ConfigurationInterface
         $rootNode = $treeBuilder->root('sellsy_api');
         $rootNode
             ->children()
-                ->scalarNode('adapter')->isRequired()->cannotBeEmpty()->defaultValue('mapper')
+                ->scalarNode('adapter')->defaultValue('mapper')
                     ->validate()
                         ->ifNotInArray(array('base', 'mapper'))
                         ->thenInvalid('Configuration "adapter" is required and can have one of the following values: base, mapper')
